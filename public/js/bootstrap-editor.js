e28b27ff92b5144e7074184cc4909bf6d18658a1
davedf <dave@deflorinier.me.uk>
2013-01-09 15:49:52 +0000

    popover menu on items

--- public/js/bootstrap-editor.js
+++ public/js/bootstrap-editor.js
@@ -1,7 +1,6 @@
 $(function(){
-    var customClasses={ selectedClass:'btn-success', hoverClass:'btn-warning' }
-	var changed=false;
-	
+    var changed=false;
+    
     function jquery_repaint_map(active_content, active_jq_map,onComplete){
       var background_jq_map=$('#map1');
       dom_repaint_entire_map(active_content,background_jq_map);
@@ -11,9 +10,37 @@ $(function(){
     }
 
     function load_content(json_object){
-	 logMapActivity('View');
+     logMapActivity('View');
       var active_content=content(json_object);
-	  $("#menuPublish").hide();
+      $("#menuPublish").hide();
+      ;
+      
+      var attachPopoverClick = function(currentLabel) {
+			currentLabel.click(function(){
+				$("#map2 .MAP_label").popover('destroy');
+	            currentLabel.popover({placement:'top',html:true,trigger:'manual',title:'Options',
+	                        content:'<ul class="nav nav-tabs nav-stacked">' +
+	                                '<li><a href="#" class="menuAdd">Add</a></li>' +
+	                                '<li><a href="#" class="menuEdit">Edit</a></li>' + 
+	                                '<li><a href="#" class="menuRemove">Remove</a></li>' +
+	                                '</ul>'})
+	            currentLabel.popover('show');
+	            $('.menuAdd').click(function(){currentLabel.popover('hide');active_content.addSubIdea(currentLabel.attr("idea"),'A cunning plan');  });
+	            $('.menuRemove').click(function(){currentLabel.popover('hide');active_content.removeSubIdea(currentLabel.attr("idea"));});
+	            $('.menuEdit').click(function(){currentLabel.popover('hide');
+	                editLabel(
+	                    currentLabel,
+	                        function(newValue){
+	                            active_content.updateTitle(currentLabel.attr("idea"),newValue);
+	                        }
+	                );
+	            });
+				
+			});
+
+        }
+      var customClasses={ selectedClass:'btn-success', hoverClass:'btn-warning',newLabelCallBack:attachPopoverClick }
+    
       var selectedId=function(){
         return $('#map2').find('.'+customClasses.selectedClass).attr('idea') || active_content.id;
       }
@@ -22,10 +49,10 @@ $(function(){
       attach_map_listeners(active_content,$('#map2'), jquery_repaint_map,customClasses);
       active_content.addEventListener("changed",function() {
         if (!changed) {
-		  $("#menuPublish").show();
-		logMapActivity('Edit');
-		}
-		changed = true;
+          $("#menuPublish").show();
+        logMapActivity('Edit');
+        }
+        changed = true;
       });
       $('#menuAdd').click(function(){
         active_content.addSubIdea(selectedId(),'A cunning plan');  
@@ -45,16 +72,18 @@ $(function(){
       $('#menuClear').click(function(){
         _.each(active_content.ideas, function(idea){ active_content.removeSubIdea(idea.id);})
       });
- 	var publishMap = function(result) {
-		var publishTime=Date.now();
-		logMapActivity('Publish',result.key);
-	
-		$("#s3form [name='file']").val("window.map="+JSON.stringify(active_content));
+      $("#map2 .MAP_label").each(function() {attachPopoverClick($(this));});
+
+    var publishMap = function(result) {
+        var publishTime=Date.now();
+        logMapActivity('Publish',result.key);
+    
+        $("#s3form [name='file']").val("window.map="+JSON.stringify(active_content));
         for (var name in result) {$('#s3form [name='+name+']').val(result[name])};
         $('#s3form').submit();
-	}
+    }
     $("#menuPublish").click(function(){
-		$.getJSON("/publishingConfig", publishMap);
+        $.getJSON("/publishingConfig", publishMap);
     });
   }
   load_content(window.map);
