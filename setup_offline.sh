cb9915e20bb88332ad47f70b2b799ba69994144d
davedf <dave@deflorinier.me.uk>
2013-01-21 08:30:09 +0000

    remove offline folders - run setup_offline.sh to setup offline filders

---
+++ setup_offline.sh
@@ -0,0 +1,7 @@
+mkdir -p public/offline
+mkdir -p public/offline
+rm -rf public/offline/*
+curl http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js > public/offline/jquery.min.js
+curl http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.3/underscore-min.js > public/offline/underscore-min.js
+curl http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js > public/offline/bootstrap.min.js
+curl http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css > public/offline/bootstrap-combined.min.css
