f350013e1b09e1a882327960be19681a186f8098
davedf <dave@deflorinier.me.uk>
2013-02-19 17:15:05 +0000

    Tidy up floating toolbar, move color picker into button

--- public/main.js
+++ public/main.js
@@ -35,7 +35,8 @@ MM.main = function (config) {
 		jQuery('#topbar').alertWidget(alert);
 		jQuery('#modalFeedback').feedbackWidget(jotForm, activityLog);
 		jQuery('#modalVote').voteWidget(activityLog, alert);
-		jQuery('#toolbarEdit [data-mm-target-property=background]').colorPicker();
+		jQuery('#toolbarEdit .updateStyle').colorPicker();
+		jQuery('#toolbarEdit .colorPicker-picker').parent('button').click(function (e) { if (e.target === this) {jQuery(this).find('.colorPicker-picker').click(); } });
 		jQuery('#toolbarEdit').mapToolbarWidget(mapModel);
 		jQuery('#floating-toolbar').floatingToolbarWidget(mapRepository, pngExporter);
 		jQuery("#listBookmarks").bookmarkWidget(mapBookmarks, alert);
