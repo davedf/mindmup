3082a6afea28efb290ebd33f9b063f71c07efa80
davedf <dave@deflorinier.me.uk>
2013-03-06 14:50:07 +0000

    to push to staging for checking on android device

--- public/topbar-widget.js
+++ public/topbar-widget.js
@@ -1,4 +1,7 @@
 /*global $, content*/
+$(document).on('touchstart.dropdown.data-api', '.dropdown-submenu > a', function (event) {
+		event.preventDefault();
+	});
 $.fn.topbarWidget = function () {
 	'use strict';
 	var element = this;
