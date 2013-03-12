25964b9a809f40de50a9111d4780158387fbe424
Gojko Adzic <gojko@gojko.com>
2013-03-12 21:10:57 +0000

    cmd+s working even when node in edit mode

--- public/mapjs-compiled.js
+++ public/mapjs-compiled.js
@@ -1271,6 +1271,10 @@ MAPJS.MapModel = function (mapRepository, layoutCalculator, titlesToRandomlyChoo
 						onCancelEdit();
 					} else if (e.which === 9) {
 						e.preventDefault();
+					} else if (e.which === 83 && (e.metaKey || e.ctrlKey)) {
+						e.preventDefault();
+						onCommit();
+						return; /* propagate to let the environment handle ctrl+s */
 					}
 					e.stopPropagation();
 				})
