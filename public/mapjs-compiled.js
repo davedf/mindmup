4cf00722480142b74ee3c7b8840f7d84b2162369
Damjan Vujnovic <damjan@samuraiprinciple.com>
2013-04-03 15:11:24 +0100

    merged with mapjs

--- public/mapjs-compiled.js
+++ public/mapjs-compiled.js
@@ -1322,6 +1322,30 @@ MAPJS.MapModel = function (mapRepository, layoutCalculator, titlesToRandomlyChoo
 	};
 	Kinetic.Global.extend(Kinetic.Connector, Kinetic.Shape);
 }());
+/*global Kinetic*/
+Kinetic.Clip = function (config) {
+	'use strict';
+	this.createAttrs();
+	Kinetic.Shape.call(this, config);
+	this.shapeType = 'Clip';
+	this._setDrawFuncs();
+};
+Kinetic.Clip.prototype.drawFunc = function (canvas) {
+	'use strict';
+	var context = canvas.getContext(),
+		xClip = this.getWidth() * 2 - this.getRadius() * 2;
+	context.beginPath();
+	context.moveTo(0, this.getClipTo());
+	context.arcTo(0, 0, this.getWidth() * 2, 0,  this.getWidth());
+	context.arcTo(this.getWidth() * 2, 0, this.getWidth() * 2, this.getHeight(),  this.getWidth());
+	context.arcTo(this.getWidth() * 2, this.getHeight(), 0, this.getHeight(), this.getRadius());
+	context.arcTo(xClip, this.getHeight(), xClip, 0, this.getRadius());
+	context.lineTo(xClip, this.getClipTo());
+	canvas.fillStroke(this);
+};
+Kinetic.Node.addGetterSetter(Kinetic.Clip, 'clipTo', 0);
+Kinetic.Node.addGetterSetter(Kinetic.Clip, 'radius', 0);
+Kinetic.Global.extend(Kinetic.Clip, Kinetic.Shape);
 /*global MAPJS, Color, _, jQuery, Kinetic*/
 /*jslint nomen: true, newcap: true, browser: true*/
 (function () {
