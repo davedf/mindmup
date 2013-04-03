65e19706005759c355d7aea4476c4ff9a4fd00ac
Gojko Adzic <gojko@gojko.com>
2013-04-03 22:52:44 +0100

    wysiwyg editor

--- public/attachment-editor-widget.js
+++ public/attachment-editor-widget.js
@@ -3,23 +3,38 @@
 $.fn.attachmentEditorWidget = function (mapModel) {
 	'use strict';
 	var element = this,
-		editor = element.find('[data-mm-role=editor]'),
-		open = function (ideaId, attachment) {
-			var contentType = attachment && attachment.contentType,
-				save = function () {
-					element.modal('hide');
-					mapModel.setAttachment('attachmentEditorWidget', ideaId, {contentType: 'text/html', content: editor.val() });
-				};
+		editorArea = element.find('[data-mm-role=editor]'),
+		wysiEditor = editorArea.wysihtml5().data('wysihtml5'),
+		ideaId,
+		keysBound,
+		save = function () {
+			element.modal('hide');
+			mapModel.setAttachment('attachmentEditorWidget', ideaId, {contentType: 'text/html', content: wysiEditor.editor.getValue() });
+		},
+		open = function (activeIdea, attachment) {
+			var contentType = attachment && attachment.contentType;
+			ideaId = activeIdea;
 			if (!contentType || contentType === 'text/html') {
-				editor.val(attachment && attachment.content);
+				wysiEditor.editor.setValue(attachment && attachment.content);
 				element.modal('show');
 			}
-			element.find('[data-mm-role=save]').click(save);
-			element.keydown('return', save);
 		};
+	element.find('[data-mm-role=save]').click(save);
+	element.keydown('return', save);
 	element.on('shown', function () {
-		editor.focus();
+		var realEditor = $('.wysihtml5-editor', $('.wysihtml5-sandbox')[0].contentDocument);
+		realEditor.focus();
+		if (!keysBound) {
+			realEditor.keydown('esc', function () {
+				element.modal('hide');
+			}).keydown('ctrl+s meta+s', function (e) {
+				save();
+				e.preventDefault();
+			});
+			keysBound = true;
+		}
 	});
+
 	mapModel.addEventListener('attachmentOpened', open);
 	return element;
 };
