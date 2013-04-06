bf1379d3c159ed43b300d7bb02d6a2a7bb1e6367
davedf <dave@deflorinier.me.uk>
2013-04-06 13:52:18 +0100

    MM.navigation retrieving and changing mapid

--- public/navigation.js
+++ public/navigation.js
@@ -1,11 +1,35 @@
-/*global MM*/
+/*global MM, observable*/
 MM.navigation = function (config) {
 	'use strict';
-	var self = this;
+	observable(this);
+	var self = this,
+		getMapIdFromHash = function () {
+			var windowHash = window && window.location && window.location.hash;
+			return windowHash && windowHash.length && windowHash.length > 1 && windowHash.substring && windowHash.substring(1);
+		};
+
 	self.currentMapId = function () {
-		var windowHash = window && window.location && window.location.hash,
-			mapIdFromHash = windowHash && windowHash.length && windowHash.length > 1 && windowHash.substring && windowHash.substring(1);
-		return mapIdFromHash || config.mapId;
+		return getMapIdFromHash() || config.mapId;
+	};
+	self.wireLinkForMapId = function (newMapId, link) {
+		if (getMapIdFromHash()) {
+			link.attr('href', '#');
+			link.click(function () {self.changeMapId(newMapId); });
+		} else {
+			link.attr('href', '/map/' + newMapId);
+		}
+	};
+	self.changeMapId = function (newMapId) {
+		if (newMapId && newMapId === self.currentMapId()) {
+			return false;
+		}
+		if (getMapIdFromHash()) {
+			window.location.hash = newMapId;
+			self.dispatchEvent('mapIdChanged', newMapId);
+			return true;
+		} else {
+			document.location = '/map/' + newMapId;
+		}
 	};
 	return self;
 };
--- test/navigation-spec.js
+++ test/navigation-spec.js
@@ -1,16 +1,93 @@
-/*global beforeEach, describe, expect, it, MM*/
+/*global beforeEach, describe, expect, it, MM, $, spyOn, jasmine*/
 describe('MM.navigation', function () {
 	'use strict';
 	var underTest;
 	beforeEach(function () {
 		underTest = new MM.navigation({mapId: 'mapIdInConfig'});
 	});
-	it('should return mapId from window address hash', function () {
-		window.location.hash = 'mapIdInHash';
-		expect(underTest.currentMapId()).toBe('mapIdInHash');
+	describe('currentMapId', function () {
+		it('should return mapId from window address hash', function () {
+			window.location.hash = 'mapIdInHash';
+			expect(underTest.currentMapId()).toBe('mapIdInHash');
+		});
+		it('should return mapId from config if there is no window address hash', function () {
+			window.location.hash = '';
+			expect(underTest.currentMapId()).toBe('mapIdInConfig');
+		});
 	});
-	it('should return mapId from config if there is no window address hash', function () {
-		window.location.hash = '';
-		expect(underTest.currentMapId()).toBe('mapIdInConfig');
+	describe('wireLinkForMapId', function () {
+		var link;
+		beforeEach(function () {
+			link = $('<a>');
+		});
+		describe('when mapId is from window address hash', function () {
+			beforeEach(function () {
+				window.location.hash = 'mapIdInHash';
+			});
+			it('should set # as href', function () {
+				underTest.wireLinkForMapId('newMapId', link);
+				expect(link.attr('href')).toBe('#');
+			});
+			it('should set click event', function () {
+				spyOn(link, 'click').andCallThrough();
+				underTest.wireLinkForMapId('newMapId', link);
+				expect(link.click).toHaveBeenCalledWith(jasmine.any(Function));
+			});
+			it('should set the link to call changeMapId when it is clicked', function () {
+				spyOn(underTest, 'changeMapId');
+				underTest.wireLinkForMapId('newMapId', link);
+				link.click();
+				expect(underTest.changeMapId).toHaveBeenCalledWith('newMapId');
+			});
+		});
+		describe('when there is no window address hash', function () {
+			beforeEach(function () {
+				window.location.hash = '';
+			});
+			it('should set /map/newMapId as href', function () {
+				underTest.wireLinkForMapId('newMapId', link);
+				expect(link.attr('href')).toBe('/map/newMapId');
+			});
+			it('should not set click event', function () {
+				spyOn(link, 'click').andCallThrough();
+				underTest.wireLinkForMapId('newMapId', link);
+				expect(link.click).not.toHaveBeenCalled();
+			});
+		});
+	});
+	describe('changeMapId', function () {
+		describe('when mapId is from window address hash', function () {
+			var listener;
+			beforeEach(function () {
+				window.location.hash = 'mapIdInHash';
+				listener = jasmine.createSpy();
+				underTest.addEventListener('mapIdChanged', listener);
+			});
+			it('should return true when mapId is not the same', function () {
+				expect(underTest.changeMapId('newMapId')).toBe(true);
+			});
+			it('should set window address hash to new mapId', function () {
+				underTest.changeMapId('newMapId');
+				expect(window.location.hash).toBe('#newMapId');
+			});
+			it('should notify listeners of newMapId', function () {
+				underTest.changeMapId('newMapId');
+				expect(listener).toHaveBeenCalledWith('newMapId');
+			});
+			it('should return false when mapId is the same', function () {
+				expect(underTest.changeMapId('mapIdInHash')).toBe(false);
+				expect(window.location.hash).toBe('#mapIdInHash');
+				expect(listener).not.toHaveBeenCalled();
+			});
+		});
+		describe('when there is no window address hash', function () {
+			beforeEach(function () {
+				window.location.hash = '';
+			});
+			it('should return false when mapId is the same', function () {
+				expect(underTest.changeMapId('mapIdInConfig')).toBe(false);
+				expect(window.location.hash).toBe('');
+			});
+		});
 	});
 });
